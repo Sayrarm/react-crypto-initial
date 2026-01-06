0efa6e5af7f86bcd718b62e3c86836dc2fe4c538
Sayrarm <minsecond@mail.ru>
2026-01-06 15:49:56 +0400

    feat: add info for cards from data.js

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,23 +1,13 @@
 import {Layout, Card, Statistic, List, Typography, Spin} from "antd";
-import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import {ArrowDownOutlined, ArrowUpOutlined} from '@ant-design/icons';
 import {useEffect, useState} from "react";
 import {fakeFetchCrypto, fetchAssets} from "../../api.js";
+import {percentDifference} from "../../util.js";
 
 const siderStyle = {
     padding: '1rem',
 };
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
-
-function percentDifference(a, b) {
-    return 100 * Math.abs( (a - b) / ( (a + b) / 2 ) )
-}
 
 export default function AppSider() {
     const [loading, setLoading] = useState(false)
@@ -27,7 +17,7 @@ export default function AppSider() {
     useEffect(() => {
         async function preload() {
             setLoading(true)
-            const { result } = await fakeFetchCrypto()
+            const {result} = await fakeFetchCrypto()
             const assets = await fetchAssets()
 
             setAssets(assets.map(asset => {
@@ -44,45 +34,43 @@ export default function AppSider() {
             setCrypto(result)
             setLoading(false)
         }
+
         preload()
     }, [])
 
-    if(loading) {
-        return <Spin fullscreen />
+    if (loading) {
+        return <Spin fullscreen/>
     }
 
     return (
         <Layout.Sider width="25%" style={siderStyle}>
-            <Card style={ {marginBottom: '1rem'} }>
-                <Statistic
-                    title="Active"
-                    value={11.28}
-                    precision={2}
-                    styles={{ content: { color: '#3f8600' } }}
-                    prefix={<ArrowUpOutlined />}
-                    suffix="%"
-                />
+            {assets.map(asset => (
+                <Card key={asset.id} style={{marginBottom: '1rem'}}>
+                    <Statistic
+                        title={asset.id}
+                        value={asset.totalAmount}
+                        precision={2}
+                        styles={{content: {color: asset.grow ? '#3f8600' : '#cf1322'}}}
+                        prefix={asset.grow ? <ArrowUpOutlined/> : <ArrowDownOutlined/>}
+                        suffix="$"
+                    />
 
-                <List
-                    size="small"
-                    dataSource={data}
-                    renderItem={item => (
-                        <List.Item>
-                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                        </List.Item>
-                    )}
-                />
-            </Card>
-            <Card>
-                <Statistic
-                    title="Idle"
-                    value={9.3}
-                    precision={2}
-                    styles={{ content: { color: '#cf1322' } }}
-                    prefix={<ArrowDownOutlined />}
-                    suffix="%"
-                />
-            </Card>
+                    <List
+                        size="small"
+                        dataSource={[
+                            {title: 'Total Profit', value: asset.totalProfit},
+                            {title: 'Asset Amount', value: asset.amount},
+                            {title: 'Difference', value: asset.growPercent},
+                        ]}
+                        renderItem={item => (
+                            <List.Item>
+                                <span>{item.title}</span>
+                                <span>{item.value}</span>
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+            ))}
         </Layout.Sider>
     )
 }
