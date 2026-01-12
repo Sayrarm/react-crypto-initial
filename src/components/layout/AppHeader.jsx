8ca9d7d5b0c8934a71f8981913a3b960509d0a82
Sayrarm <minsecond@mail.ru>
2026-01-12 16:17:08 +0300

    feat: add header. add select and button

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,16 +1,75 @@
-import {Layout} from "antd";
+import {Layout, Select, Space, Button} from "antd";
+import {useCrypto} from "../../context/crypto-context.jsx";
 
 const headerStyle = {
+    width: "100%",
     textAlign: 'center',
-    color: '#fff',
     height: 60,
-    paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: '#4096ff',
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
 };
 
+const options = [
+    {
+        label: 'Happy',
+        value: 'happy',
+        emoji: '😄',
+        desc: 'Feeling Good',
+    },
+    {
+        label: 'Sad',
+        value: 'sad',
+        emoji: '😢',
+        desc: 'Feeling Blue',
+    },
+    {
+        label: 'Angry',
+        value: 'angry',
+        emoji: '😡',
+        desc: 'Furious',
+    },
+    {
+        label: 'Cool',
+        value: 'cool',
+        emoji: '😎',
+        desc: 'Chilling',
+    },
+    {
+        label: 'Sleepy',
+        value: 'sleepy',
+        emoji: '😴',
+        desc: 'Need Sleep',
+    },
+];
+
 export default function AppHeader() {
+    const { crypto } = useCrypto()
     return (
-        <Layout.Header style={headerStyle}>Header</Layout.Header>
+        <Layout.Header style={headerStyle}>
+            <Select
+                mode="multiple"
+                style={{width: '250px'}}
+                placeholder="press / to open"
+                options={ crypto.map(coin => ({
+                    label: coin.name,
+                    value: coin.id,
+                    icon: coin.icon,
+                })) }
+                optionRender={(option) => (
+                    <Space>
+                        <img
+                            style={{width: 20}}
+                            src={option.data.icon}
+                            alt={option.data.label}/>
+                            {' '}
+                            {option.data.label}
+                    </Space>
+                )}
+            />
+
+            <Button type="primary">Primary Button</Button>
+        </Layout.Header>
     )
 }
--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect,  } from "react";
+import {createContext, useState, useEffect, useContext,} from "react";
 import {fakeFetchCrypto, fetchAssets} from "../api.js";
 import {percentDifference} from "../util.js";
 
@@ -44,3 +44,7 @@ export function CryptoContextProvider({ children }) {
 }
 
 export default CryptoContext
+
+export function useCrypto() {
+    return useContext(CryptoContext)
+}
